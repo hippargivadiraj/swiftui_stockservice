37327777b1cceff507b50a75c8955f17429abc8c
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-01-25 22:44:35 -0800

    Initial Commit

---
+++ StocksService/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StocksService
+//
+//  Created by Vadiraj Hippargi on 1/25/20.
+//  Copyright © 2020 Vadiraj Hippargi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
